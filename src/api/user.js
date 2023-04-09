e17a5e712dab62947ae15f98c51a5236ab6300fb
jiahao.wu <1696764441@qq.com>
2023-04-09 22:59:14 +0800

    用户注册

--- src/api/user.js
+++ src/api/user.js
@@ -1,8 +1,33 @@
 import request from './request'
 
-export function getCaptcha(){
+/**
+ * 
+ * @returns 获取验证码
+ */
+export function getCaptcha() {
     return request({
         url: 'res/captcha',
         method: 'GET',
+    })
+}
+
+
+/**
+ * 
+ * @param {String} loginId 用户登录账号
+ * @returns 
+ */
+export function userIsExit(loginId) {
+    return request({
+        url: `/api/user/userIsExist/${loginId}`,
+        method: 'GET',
+    })
+}
+
+export function addUser(newUserInfo){
+    return request({
+        url: `/api/user`,
+        method: 'POST',
+        data: newUserInfo
     })
 }
