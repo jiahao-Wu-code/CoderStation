8b9310147de7d42898379d06a5c49b633a7838a7
jiahao.wu <1696764441@qq.com>
2023-04-12 23:18:37 +0800

    问答列表组件

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import styles from '../css/PageHeader.module.css'
+
+/**
+ * 每一页的页头
+ * @returns 
+ */
+export default function PagerHeader(props) {
+    return (
+        <div className={styles.row}>
+            <div className={styles.pageHeader}>
+                {props.title}
+            </div>
+            {/* 分类选择 */}
+        </div>
+    )
+}
